121a37c3e67bd6fb047ab21870a0a1c647a6256d
RubyTuesday77 <105758468+RubyTuesday77@users.noreply.github.com>
2022-08-06 12:26:14 -0400

    activity completed

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 class Casino {
-    // Write code here
-};
+    constructor(name) {
+        this.name = name;
+        this.timesPlayed = 0;
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed += 1;
+        if(Math.random() <= 0.5) {
+            console.log(this.name + " wins!")
+        } else {
+            console.log("You won " + betAmount * (this.timesPlayed + 1))
+        }
+    }
+}
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
